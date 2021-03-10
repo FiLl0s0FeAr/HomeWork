26718f78cb2b90d864f10a0a4d9c2aacc0224266
Sergey <45178548+FiLl0s0FeAr@users.noreply.github.com>
2021-03-10 19:17:18 +0200

    Update TestNGCraftOfExile.java

--- src/test/java/TestNGCraftOfExile.java
+++ src/test/java/TestNGCraftOfExile.java
@@ -14,9 +14,7 @@ public class TestNGCraftOfExile {
     @BeforeMethod(description = "Configure something before test")
     public void setBaseURLCraftOfExile(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(false);
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.get(baseURLCraftOfExile);
     }
 
