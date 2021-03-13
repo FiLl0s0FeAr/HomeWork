215fac50c534cf10a03d7307a2428df8d4fd0bc3
FiLl0s0FeAr <vivasu5@ukr.net>
2021-03-13 11:48:58 +0200

    Appache practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,3 @@
-import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Scanner;
